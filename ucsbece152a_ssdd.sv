8e86f5e50aada753db651d0295dbf9a34d1e8770
tanishka076 <tanishka@ucsb.edu>
2026-05-15 14:29:18 -0700

    Rename module and update input/output signals

--- ucsbece152a_ssdd.sv
+++ ucsbece152a_ssdd.sv
@@ -1,19 +1,19 @@
-module seven_seg_decoder (
-    input  logic [2:0] digit_i,
-    output logic [6:0] seg_o
+module ucsbece152a_ssdd (
+    input  logic [2:0] data_i,
+    output logic [6:0] seven_segment_display_o
 );
 
 always_comb begin
-    case (digit_i)
-        3'd0: seg_o = 7'b1111110;
-        3'd1: seg_o = 7'b0110000;
-        3'd2: seg_o = 7'b1101101;
-        3'd3: seg_o = 7'b1111001;
-        3'd4: seg_o = 7'b0110011;
-        3'd5: seg_o = 7'b1011011;
-        3'd6: seg_o = 7'b1011111;
-        3'd7: seg_o = 7'b1110000;
-        default: seg_o = 7'b0000000;
+    case (data_i)
+        3'h0: seven_segment_display_o = 7'b1111110;
+        3'h1: seven_segment_display_o = 7'b0110000;
+        3'h2: seven_segment_display_o = 7'b1101101;
+        3'h3: seven_segment_display_o = 7'b1111001;
+        3'h4: seven_segment_display_o = 7'b0110011;
+        3'h5: seven_segment_display_o = 7'b1011011;
+        3'h6: seven_segment_display_o = 7'b1011111;
+        3'h7: seven_segment_display_o = 7'b1110000;
+        default: seven_segment_display_o = 7'b0000000;
     endcase
 end
 
